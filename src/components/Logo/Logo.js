4105f9cd7ff4414380fd295e20fafb2dbb229d38
Jorge <jorgediazok@gmail.com>
2020-08-22 16:03:29 -0300

    backdrop transition

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -4,7 +4,10 @@ import burgerLogo from '../../assets/images/burger-logo.png';
 import classes from './Logo.module.css';
 
 const logo = (props) => (
-  <div className={classes.Logo} style={{ height: props.height }}>
+  <div
+    className={classes.Logo}
+    style={{ height: props.height, marginBottom: props.marginbt }}
+  >
     <img src={burgerLogo} alt="MyBurger" />
   </div>
 );
